339fe9f09884598a24921da863200ddca2f6cca7
Shashi <shashiran12345@gmail.com>
2026-07-22 18:16:14 +0530

    firstTest

--- tests/HomePageTest.spec.js
+++ tests/HomePageTest.spec.js
@@ -8,3 +8,7 @@ test('Home Page',async ({page}) => {
     await page.close();
 
 })
+
+// to run the test case is to all the test in one go cmd is  npx playwright test      //--- all in headless mode
+// to run only one test file cmd is  npx playwright test HomePageTest.spec.js --headed 
+// npx playwright test tests/HomePageTest.spec.js for specific browser
